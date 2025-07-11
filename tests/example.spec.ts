e3749e4deada7111599e0aadd2970d37f0c06663
sanxjay <jayhawkv16@gmail.com>
2025-07-11 23:24:11 +0530

    Initial Playwright Project On Linux (Manjaro xfce)

---
+++ tests/example.spec.ts
@@ -0,0 +1,15 @@
+// tests/example.spec.ts
+import { test, expect } from '@playwright/test';
+
+test('homepage has title and get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
